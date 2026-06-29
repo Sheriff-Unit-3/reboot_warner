12086de83339fe7be19ca24bea65ca6c3c212377
Sheriff_U3 <210896603+Sheriff-Unit-3@users.noreply.github.com>
2026-06-28 22:01:07 -0500

    minetest -> core (#3)

--- init.lua
+++ init.lua
@@ -1,23 +1,21 @@
 -- Simple reboot warning mod
-
-local ie = minetest.request_insecure_environment()
-
+local ie = core.request_insecure_environment()
 local timer = 0
-local warning_file = minetest.get_worldpath().."/reboot_warning"
+local warning_file = core.get_worldpath().."/reboot_warning"
 
-minetest.register_globalstep(function(dtime)
+core.register_globalstep(function(_)
 	if timer > 600 then -- 600 tenths = 1 minute
 		timer = 0
 		local f = io.open(warning_file, "r")
 		if f then
 			io.close(f)
 			ie.os.remove(warning_file)
-			for _, player in ipairs(minetest.get_connected_players()) do
+			for _, player in ipairs(core.get_connected_players()) do
 				local playername = player:get_player_name()
 				if playername then
 					notice.send(playername,
 						"*** IMPORTANT NOTICE: | The server will be | rebooting for its nightly | backup in a few minutes!")
-					minetest.log("Issued reboot warning to "..playername)
+					core.log("Issued reboot warning to "..playername)
 				end
 			end
 		end
