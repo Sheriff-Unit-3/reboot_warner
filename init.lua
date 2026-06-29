1532559a5f0feafe9ac3712f56f51d4b97d10643
Sheriff-Unit-3 <210896603+Sheriff-Unit-3@users.noreply.github.com>
2026-06-28 20:25:53 -0500

    Translate reboot notice
    Tranlsations for French, German, Italian, and Spanish are included.

--- init.lua
+++ init.lua
@@ -1,7 +1,8 @@
 -- Simple reboot warning mod
 local ie = core.request_insecure_environment()
 local timer = 0
-local warning_file = core.get_worldpath().."/reboot_warning"
+local warning_file = core.get_worldpath() .. "/reboot_warning"
+local S = core.get_translator("reboot_warner")
 
 core.register_globalstep(function(_)
 	if timer > 600 then -- 600 tenths = 1 minute
@@ -13,9 +14,13 @@ core.register_globalstep(function(_)
 			for _, player in ipairs(core.get_connected_players()) do
 				local playername = player:get_player_name()
 				if playername then
-					notice.send(playername,
-						"*** IMPORTANT NOTICE: | The server will be | rebooting for its nightly | backup in a few minutes!")
-					core.log("Issued reboot warning to "..playername)
+					notice.send(
+						playername,
+						S(
+							"*** IMPORTANT NOTICE: | The server will be | rebooting for its nightly | backup in a few minutes!"
+						)
+					)
+					core.log("Issued reboot warning to " .. playername)
 				end
 			end
 		end
